0233ec3c65ce459c262ee1d7d2b65658d3c55b16
Joel <2016014076@unfv.edu.pe>
2025-03-16 23:50:38 -0500

    Remove BallButton component and its index export; clean up three.d.ts module declaration

--- src/types/three.d.ts
+++ src/types/three.d.ts
@@ -1,5 +1,3 @@
-import * as THREE from "three";
-
 declare module "three" {
   export class TruncatedIcosahedronGeometry extends PolyhedronGeometry {
     constructor(radius?: number, detail?: number);
